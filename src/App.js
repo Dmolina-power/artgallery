970d79a00ff82f17c2c37d7c751e915bbc463f7b
daniel molina <bigdpower@icloud.com>
2023-02-09 01:44:17 -0600

    added navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "./Components/Navbar";
+import './App.css';
+
+function App() {
+  
+  
+  return (
+    <div className="App">
+    
+    <Nav />
+    </div>
+  );
+}
+
+export default App;
